9fc390e90e79b0a8107853c86a3399409b7d55f4
Daniel PC <daniel.barczynski@onet.pl>
2022-04-24 18:22:08 +0200

    prototype

---
+++ Kserokopiarka/Zadanie1/Copier.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Zadanie1
+{
+    internal class Copier
+    {
+        public int Counter { get; set; }
+        public int PrintCounter { get; set; }
+        public int ScanCounter { get; set; }
+        public void PowerOn() { }
+        public void PowerOff() { }
+        public void Print() { }
+        public void Scan() { }
+        public void ScanAndPrint() { }
+    }
+}
