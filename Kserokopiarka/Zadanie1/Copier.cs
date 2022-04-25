2125d8216afb42ae3f15c0a4e3dacbdce6fea4af
Daniel PC <daniel.barczynski@onet.pl>
2022-04-25 14:54:49 +0200

    zadanie1 first attempt

--- Kserokopiarka/Zadanie1/Copier.cs
+++ Kserokopiarka/Zadanie1/Copier.cs
@@ -8,13 +8,55 @@ namespace Zadanie1
 {
     internal class Copier
     {
+        private int i = 0;
+        private bool isOn;
         public int Counter { get; set; }
         public int PrintCounter { get; set; }
         public int ScanCounter { get; set; }
-        public void PowerOn() { }
-        public void PowerOff() { }
-        public void Print() { }
-        public void Scan() { }
-        public void ScanAndPrint() { }
+        public void PowerOn() {
+            if (isOn == false)
+            {
+                isOn = true;
+                Console.WriteLine("Urządzenie włączone");
+            }
+        }
+        public void PowerOff() {
+            if (isOn)
+            {
+                isOn = false;
+                Console.WriteLine("Urządzenie wyłączone");
+            }
+        }
+        public void Print(string document) {
+            if (isOn)
+            {
+                DateTime date = DateTime.Now;
+                Console.WriteLine($"{date} Print: {document}");
+            }
+        }
+        public void Scan(string document) {
+            if (isOn)
+            {
+                DateTime date = DateTime.Now;
+                if (document.Contains(".pdf"))
+                    document = "PDFScan" + document;
+                else if (document.Contains(".jpg"))
+                    document = "ImageScan" + document;
+                else
+                    document = "TextScan" + document;
+                Console.WriteLine($"{date} Scan: {document}{++i}");
+            }
+        }
+        public void ScanAndPrint(string document) { 
+            if (isOn)
+            {
+                DateTime date = DateTime.Now;
+                if (document.Contains(".jpg"))
+                {
+                    Console.WriteLine($"{date} Scan: {document}{++i}");
+                    Console.WriteLine($"{date} Print: {document}");
+                }
+            }       
+        }
     }
 }
--- Kserokopiarka/Zadanie1/Program.cs
+++ Kserokopiarka/Zadanie1/Program.cs
@@ -1,4 +1,5 @@
 ﻿using ver1;
+using Zadanie1;
 
 class Program
 {
@@ -6,15 +7,20 @@ class Program
     {
         var xerox = new Copier();
         xerox.PowerOn();
+        //xerox.PowerOn();
+        //xerox.PowerOff();
         IDocument doc1 = new PDFDocument("aaa.pdf");
-        xerox.Print(in doc1);
+        //xerox.Print(in doc1);
 
         IDocument doc2;
-        xerox.Scan(out doc2);
+        //xerox.Scan(out doc2);
 
         xerox.ScanAndPrint();
         System.Console.WriteLine(xerox.Counter);
         System.Console.WriteLine(xerox.PrintCounter);
         System.Console.WriteLine(xerox.ScanCounter);
+        xerox.Print("aaa.pdf");
+        xerox.Scan("aaa.jpg");
+        xerox.Scan("aaa.txt");
     }
 }
