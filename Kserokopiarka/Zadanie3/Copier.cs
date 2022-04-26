7188226dddad2d97f8f04ccfb25207a515bea977
Daniel Barczynski <daniel.barczynski@microsoft.wsei.edu.pl>
2022-04-26 17:01:43 +0200

    init

---
+++ Kserokopiarka/Zadanie3/Copier.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using ver1;
+using static ver1.IDevice;
+
+namespace Zadanie1
+{
+    public class Copier : BaseDevice, IPrinter, IScanner
+    {
+        public int PrintCounter { get; set; } = 0;
+        public int ScanCounter { get; set; } = 0;
+
+        public void Print(in IDocument document)
+        {
+            if (state == State.on)
+            {
+                DateTime date = DateTime.Now;
+                PrintCounter++;
+                Console.WriteLine($"{date} Print: {document.GetFileName()}");
+            }
+        }
+        public void Scan(out IDocument document)
+        {
+            Scan(out document, IDocument.FormatType.JPG);
+        }
+
+        public void Scan(out IDocument document, IDocument.FormatType formatType)
+        {
+            DateTime date = DateTime.Now;
+            switch (formatType)
+            {
+                case IDocument.FormatType.PDF:
+                    document = new PDFDocument($"PDFScan{++ScanCounter}.pdf");
+                    break;
+                case IDocument.FormatType.TXT:
+                    document = new TextDocument($"TextScan{++ScanCounter}.txt");
+                    break;
+                default:
+                    document = new ImageDocument($"ImageScan{++ScanCounter}.jpg");
+                    break;
+            }
+            if (state == State.on)
+                Console.WriteLine($"{date} Scan: {document.GetFileName()}");
+        }
+
+        public void ScanAndPrint()
+        {
+            if (state == State.on)
+            {
+                Scan(out IDocument document, IDocument.FormatType.JPG);
+                Print(document);
+            }
+        }
+    }
+}
