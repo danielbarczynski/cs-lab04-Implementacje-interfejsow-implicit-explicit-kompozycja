02184a1a1dc6351dfc07aca6c98e3c7f3779ef09
Daniel Barczynski <daniel.barczynski@microsoft.wsei.edu.pl>
2022-04-26 14:56:34 +0200

    zadanie2 init

---
+++ Kserokopiarka/Zadanie2/MultiFunctionalDevice.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using ver1;
+using Zadanie1;
+
+namespace Zadanie2
+{
+    public class MultiFunctionalDevice : Copier, IFax, IScanner, IPrinter
+    {
+        
+    }
+}
